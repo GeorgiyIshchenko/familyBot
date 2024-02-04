dcdb8ed697be7c946f54b2d7512287cb32b9c431
Dm I <gogaigd2005@gmail.com>
2024-02-04 20:48:59 +0300

    changed main.py

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler("events", get_family_events))
     application.add_handler(CommandHandler("update", update_events))
     # ques
-    application.job_queue.run_repeating(callback=send_event_notifications, interval=3)
+    application.job_queue.run_repeating(callback=send_event_notifications, interval=24 * 60 * 60)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
