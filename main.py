c57297f89f4e3fc8c4ad61d3d171bd4c19766eb7
Dm I <gogaigd2005@gmail.com>
2024-02-05 20:57:14 +0300

    'timeout5'

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 if __name__ == "__main__":
     # Bot Setup
-    application = Application.builder().token(BOT_TOKEN).read_timeout(15).get_updates_read_timeout(40).build()
+    application = Application.builder().token(BOT_TOKEN).read_timeout(40).get_updates_read_timeout(40).connect_timeout(
+        40).write_timeout(40).get_updates_connect_timeout(40).build()
     # handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("events", get_family_events))
@@ -23,4 +24,5 @@ if __name__ == "__main__":
     # ques
     application.job_queue.run_repeating(callback=send_event_notifications, interval=24 * 60 * 60)
 
-    application.run_polling(allowed_updates=Update.ALL_TYPES, poll_interval=0.5, timeout=20)
+    application.run_polling(allowed_updates=Update.ALL_TYPES, poll_interval=0.5, timeout=20, read_timeout=20,
+                            write_timeout=20)
